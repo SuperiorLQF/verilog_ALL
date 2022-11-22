0c3032a1cd78ff15017f76edd552dece4311e501
SuperiorLQF <superiorlqf@gmail.com>
2022-11-22 20:14:19 +0800

    牛客网上的FIFO评判有点问题,FULL和EMPTY应该为wire类型

--- [牛客]同步FIFO/SCFIFO.v
+++ [牛客]同步FIFO/SCFIFO.v
@@ -13,8 +13,8 @@ module sfifo#(
     input 			 		rinc	,
     input 		[WIDTH-1:0]	wdata	,
 
-    output reg				wfull	,
-    output reg				rempty	,
+    output wire				wfull	,
+    output wire				rempty	,
     output wire [WIDTH-1:0]	rdata
   );
   reg     [$clog2(DEPTH):0]   wr_addr,
@@ -29,7 +29,8 @@ module sfifo#(
   begin//写
     if(~rst_n)
       wr_addr<='b0;
-    else if(~wfull&&winc)
+    else if(~wfull&&winc)//给外部控制器的要求
+    //如果写满后输入FIFO的winc不能为高，不然虽然FIFO层地址不增加，但是RAM层的最高位地址会被覆盖
       wr_addr<=wr_addr+1'b1;
   end
   always @(posedge clk,negedge rst_n)
@@ -53,19 +54,8 @@ module sfifo#(
                  .raddr(rd_addr_true),
                  .rdata(rdata)
                );
-  always @(posedge clk or negedge rst_n)
-  begin
-    if(~rst_n)
-    begin
-      rempty<=1'b0;
-      wfull<=1'b0;
-    end
-    else
-    begin
-      rempty<=(rd_addr==wr_addr)?1'b1:1'b0;
-      wfull<=((rd_addr_msb!=wr_addr_msb)&&
-              (rd_addr_true==wr_addr_true))?1'b1:1'b0;
-    end
 
-  end
+assign  rempty=(rd_addr==wr_addr)?1'b1:1'b0;
+assign  wfull=((rd_addr_msb!=wr_addr_msb)&&
+              (rd_addr_true==wr_addr_true))?1'b1:1'b0;
 endmodule
