72df375b200964f153b3074b17d916053ceb2e25
superiorlqf <superiorlqf@gmail.com>
2022-12-05 20:25:53 +0800

    初步测试完毕arbiter,但是一些时序关系需要2个甚至多个模块联调

--- MCDF/arbiter/arbiter.v
+++ MCDF/arbiter/arbiter.v
@@ -38,17 +38,104 @@ module arbiter
     output  reg     [2:0]   a2f_pkglen_sel_o    //[-slv]slvx_pkglen_i
 );
 /*****************<中间信号>*********************/
-
-
+wire    [3:0]   ch0_prio_syn,//各通道综合优先级，计算方法见下
+                ch1_prio_syn,
+                ch2_prio_syn;
+reg     [5:0]   channel_prio;//通道优先级综合排序寄存器[5:4最高优先级通道序号][3：2次高优先级通道序号][1：0最低优先级通道序号]
+wire    [2:0]   slvx_req;//输入请求集中,!!!写成向量比数组更好，向量可以查看值，防止出错
+assign  slvx_req[0]=slv0_req_i;
+assign  slvx_req[1]=slv1_req_i;
+assign  slvx_req[2]=slv2_req_i;
+/*****************<综合优先级生成电路：组合逻辑>*********************/
+assign ch0_prio_syn=({2'b0,slv0_prio_i}<<2)+'d0;//[3:2是设置prio][1：0是自有prio，3：2相等时才起作用]
+assign ch1_prio_syn=({2'b0,slv1_prio_i}<<2)+'d1;
+assign ch2_prio_syn=({2'b0,slv2_prio_i}<<2)+'d2;
+/*****************<优先级整合逻辑>*********************/
+//根据综合优先级得出最优先通道（数值最小）
+always @(*) begin//综合优先级生成电路保证不可能相等
+    if(ch0_prio_syn>ch1_prio_syn)begin
+        if(ch1_prio_syn>ch2_prio_syn)
+            channel_prio<=6'b10_01_00;
+        else begin
+            if(ch0_prio_syn>ch2_prio_syn)
+                channel_prio<=6'b01_10_00;
+            else
+                channel_prio<=6'b01_00_10;
+        end
+    end
+    else begin
+        if(ch1_prio_syn<ch2_prio_syn)
+            channel_prio<=6'b00_01_10;
+        else begin
+            if(ch0_prio_syn<ch2_prio_syn)
+                channel_prio<=6'b00_10_01;
+            else
+                channel_prio<=6'b10_00_01;
+        end        
+    end
+end
 /*****************<时序电路>*********************/
+//用于产生仲裁信号 a2f_id_o !!!如果都没准备号，就发送11
 always @(posedge clk_i or negedge rstn_i) begin
     if(~rstn_i)begin//输出全部置0
-        a2s0_ack_o<=0;
-        a2s1_ack_o<=0;
-        a2s2_ack_o<=0;
-        a2f_val_o<=0;
-        a2f_id_o<=0;
-        a2f_data_o<=0;
+        a2f_id_o<=2'b11;//进入default间接置0
     end
+    else if(f2a_id_req_i)begin//当上级(formatter)命令可以发送包!!!f2a_id_req_i应当时单周期信号，不能持续
+        if(slvx_req[channel_prio[5:4]])//先查看优先级最高的slave是否请求发送
+            a2f_id_o<=channel_prio[5:4];
+        else if(slvx_req[channel_prio[3:2]])
+            a2f_id_o<=channel_prio[3:2];
+        else if(slvx_req[channel_prio[1:0]])
+            a2f_id_o<=channel_prio[1:0];
+        else
+            a2f_id_o<=2'b11;
+    end
+    else
+        a2f_id_o<=a2f_id_o;
+end
+
+/*****************<多路选择器部分>*********************/
+always @(*) begin
+    case(a2f_id_o)
+    2'b00:begin
+        a2f_val_o=slv0_val_i;
+        a2f_data_o=slv0_data_i;  
+        a2f_pkglen_sel_o=slv0_pkglen_i;
+        
+        a2s0_ack_o=f2a_ack_i;
+        a2s1_ack_o=0;
+        a2s2_ack_o=0;
+    end
+
+    2'b01:begin
+        a2f_val_o=slv1_val_i;
+        a2f_data_o=slv1_data_i; 
+        a2f_pkglen_sel_o=slv1_pkglen_i; 
+        
+        a2s0_ack_o=0;
+        a2s1_ack_o=f2a_ack_i;
+        a2s2_ack_o=0;
+    end
+
+    2'b10:begin
+        a2f_val_o=slv2_val_i;
+        a2f_data_o=slv2_data_i; 
+        a2f_pkglen_sel_o=slv2_pkglen_i;
+        
+        a2s0_ack_o=0;
+        a2s1_ack_o=0;
+        a2s2_ack_o=f2a_ack_i;
+    end
+
+    2'b11:begin//default
+        a2f_val_o=0;
+        a2f_data_o=0; 
+        a2f_pkglen_sel_o=0;
+        
+        a2s0_ack_o=0;
+        a2s1_ack_o=0;
+        a2s2_ack_o=0;
+    end
+    endcase
 end
 endmodule
---
+++ MCDF/arbiter/arbiter_tb.v
@@ -0,0 +1,137 @@
+`timescale 1ns/1ns
+`include "arbiter.v"
+/*************************<端口声明>*********************/
+module arbiter_tb;
+reg             clk_i,
+                rstn_i;
+/********<control register 接口>*******/
+reg     [1:0]   slv0_prio_i,
+                slv1_prio_i,
+                slv2_prio_i;
+reg     [2:0]   slv0_pkglen_i,
+                slv1_pkglen_i,
+                slv2_pkglen_i;
+/********<slave 接口>*********************/
+reg     [31:0]  slv0_data_i,
+                slv1_data_i,
+                slv2_data_i;
+reg             slv0_req_i,
+                slv1_req_i,
+                slv2_req_i,    
+                slv0_val_i,
+                slv1_val_i,
+                slv2_val_i;
+/********<formatter 接口>*********************/    
+reg             f2a_id_req_i,       //formatter请求接收
+                f2a_ack_i;
+/********<slave 接口>*********************/    
+wire            a2s0_ack_o,         //[-slv]f2a_ack_i
+                a2s1_ack_o,
+                a2s2_ack_o,
+/********<formatter 接口>*********************/
+                a2f_val_o;         //[-slv]slvx_val_i,包有效包络信号
+wire    [1:0]   a2f_id_o;           //通道编号
+wire    [31:0]  a2f_data_o;         //[-slv]slvx_data_i
+wire    [2:0]   a2f_pkglen_sel_o;    //[-slv]slvx_pkglen_i
+
+/********************<实例化arbiter>*********************/
+arbiter ar1(
+    clk_i,
+    rstn_i,
+
+    slv0_prio_i,
+    slv1_prio_i,
+    slv2_prio_i,
+    slv0_pkglen_i,
+    slv1_pkglen_i,
+    slv2_pkglen_i,
+
+    slv0_data_i,
+    slv1_data_i,
+    slv2_data_i,
+    slv0_req_i,
+    slv1_req_i,
+    slv2_req_i,    
+    slv0_val_i,
+    slv1_val_i,
+    slv2_val_i,
+
+    f2a_id_req_i,   
+    f2a_ack_i,
+
+    a2s0_ack_o,     
+    a2s1_ack_o,
+    a2s2_ack_o,
+
+    a2f_val_o,     
+    a2f_id_o,       
+    a2f_data_o,     
+    a2f_pkglen_sel_o
+);
+initial begin
+/*********<初始化>**********/
+    clk_i<=0;
+    rstn_i<=0;//
+
+    slv0_prio_i<=2'b11;//
+    slv1_prio_i<=2'b01;//这个是最优先通道
+    slv2_prio_i<=2'b01;//次优先通道
+    slv0_pkglen_i<=2'b10;//
+    slv1_pkglen_i<=2'b01;//数据长8
+    slv2_pkglen_i<=2'b00;//数据长4
+
+    slv0_data_i<=$random;
+    slv1_data_i<=$random;
+    slv2_data_i<=$random;
+    slv0_req_i<=0;
+    slv1_req_i<=0;
+    slv2_req_i<=0; 
+    slv0_val_i<=0;
+    slv1_val_i<=0;
+    slv2_val_i<=0;
+
+    f2a_id_req_i<=0;//短时信号，在slvx_req_i就绪之后  
+    f2a_ack_i<=0;//在slvx_req_i就绪之后
+    $dumpfile("arbiter.vcd");
+    $dumpvars;
+/*********<生成波形>**********/
+#25     rstn_i<=1;
+#20     slv0_req_i<=1;
+        slv1_req_i<=1;
+        slv2_req_i<=1;
+@(posedge clk_i)begin
+    f2a_id_req_i<=1;//两个短时信号
+end 
+@(posedge clk_i)begin
+    f2a_id_req_i<=0;
+    f2a_ack_i<=1;     
+end
+@(posedge clk_i)begin
+    f2a_ack_i<=0;     
+end
+ 
+        repeat(8)begin
+            @(posedge clk_i)begin
+                slv0_data_i<=$random;
+                slv1_data_i<=$random;
+                slv2_data_i<=$random;
+                slv0_val_i<=0;
+                slv1_val_i<=1;
+                slv2_val_i<=0;                
+            end
+        end
+@(posedge clk_i)begin
+    slv0_data_i<=$random;
+    slv1_data_i<=$random;
+    slv2_data_i<=$random;
+    slv0_val_i<=0;
+    slv1_val_i<=0;
+    slv2_val_i<=0;
+    slv0_req_i<=0;
+    slv1_req_i<=0;
+    slv2_req_i<=0;                
+end
+#2000   $finish;
+end
+always #10 clk_i<=~clk_i;
+endmodule
