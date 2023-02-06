cbed92b2cd0b8a2e2bf5e15bcdd70a0eac757e15
superior <superiorlqf@gmail.com>
2023-02-06 15:54:02 +0800

    new file:   MCDF.zip 	modified:   MCDF/control_register/control_register.v.out 	deleted:    MCDF/control_register/control_register.vcd 	modified:   MCDF/control_register/control_register_tb.v.out 	new file:   MCDF/control_register/testout 	new file:   MCDF/control_register/testwave1.gtkw 	modified:   MCDF/formatter/formatter.v.out 	new file:   "\345\220\214\346\255\245\345\274\202\346\255\245\347\224\265\350\267\257/\345\277\253\346\227\266\351\222\237\345\237\237\346\216\247\345\210\266\344\277\241\345\217\267\345\220\214\346\255\245\345\210\260\346\205\242\346\227\266\351\222\237\345\237\237/f2s_control.v" 	new file:   "\345\220\214\346\255\245\345\274\202\346\255\245\347\224\265\350\267\257/\345\277\253\346\227\266\351\222\237\345\237\237\346\216\247\345\210\266\344\277\241\345\217\267\345\220\214\346\255\245\345\210\260\346\205\242\346\227\266\351\222\237\345\237\237/f2s_control.v.out"

---
+++ 同步异步电路/快时钟域控制信号同步到慢时钟域/f2s_control.v
@@ -0,0 +1,44 @@
+/*参考刘清帆CSDN:【SOC架构】（一）同步与异步信号*/
+/*快时钟域控制信号同步到慢时钟域*/
+module f2s_control(
+    input       adat,
+                rst,
+                aclk,
+                bclk,
+    output  reg bdat
+);
+reg     adat1,
+        bdat1,  bdat2,  bdat3,
+        abdat1, abdat2;
+//实例化慢时钟域同步器
+always @(posedge bclk) begin
+    {bdat2,bdat1}<={bdat1,adat1};
+end
+//实例化快时钟域同步器
+always @(posedge aclk) begin
+    {abdat2,abdat1}<={abdat1,bdat2};
+end
+//实例化快时钟域同步慢时钟域方法：缩短
+always @(posedge bclk or negedge rst) begin
+    if(~rst)
+        bdat3<=1'b0;
+    else
+        bdat3<=bdat2;
+end
+always @(bdat3,bdat2) begin
+    bdat<=({bdat3,bdat2}==2'b01)?1'b1:1'b0;
+end
+
+
+//实例化慢时钟域同步快时钟域方法：反馈延时
+always @(posedge aclk or negedge rst) begin
+    if(~rst)
+        adat1<=1'b0;
+    else    //adat触发，abdat2关闭
+        if(abdat2)
+            adat1<=1'b0;
+        else
+            if(adat)
+                adat1<=1'b1;
+end
+endmodule
